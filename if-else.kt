bd5c8c85bdf0795da5fef15da7221d90fb2959cd
hi-shadow <vazagautam54@gmail.com>
2022-12-14 04:37:55 +0530

    Inital commit

---
+++ if-else.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var age= 20
+    
+    if(age <18)
+    {
+        println("You are underage , DRIVING IS ILLEGAL FOR YOU")
+    }
+    else if(age == 18)
+    {
+        println("First Applied for Drving Licsence")
+    }
+    else{
+        println("tame driving kari sako chho")
+    }
+
+}
---
+++ variables.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    //variable declaration and defination [declare and assign the value]
+    var a = 10
+    var b = 20
+    var c = a+b
+    println(c)
+    
+
+    
+}
