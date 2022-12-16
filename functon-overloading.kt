2c68e982f13e503c9fa8cfbff2c85a88069bfeef
hi-shadow <vazagautam54@gmail.com>
2022-12-16 17:12:50 +0530

    Initial Commmit

---
+++ functon-overloading.kt
@@ -0,0 +1,12 @@
+fun sum(num 1 : Int , num2 :Int) : Int{
+    var sum = num +num2
+    return sum
+}
+fun sum(no : Double , no2 : Double) : Double{
+    var result = no + no2
+    return result
+}
+fun main() {
+    sum(10,30)
+    sum(10.00 , 20.00)
+}
